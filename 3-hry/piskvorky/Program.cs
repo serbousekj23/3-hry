69ea376e6c64b11c74a218760f4e2af255d013ce
Honza Serbousek <serbousekj23@spstrutnov.cz>
2024-10-04 09:35:16 +0200

    piskvorky zaklad

--- 3-hry/piskvorky/Program.cs
+++ 3-hry/piskvorky/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("herni pole:");
+            Console.WriteLine("-------------");
+            Console.WriteLine("|-1-|-2-|-3-|");
+            Console.WriteLine("|-4-|-5-|-6-|");
+            Console.WriteLine("|-7-|-8-|-9-|");
+            Console.WriteLine("-------------");
+
+            Console.WriteLine("hrac 1 vyber pole: ");
+            string hrac1 = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
