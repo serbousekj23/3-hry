be962e37e1ec4030649c92a666e4c2dac0a900ba
Honza Serbousek <serbousekj23@spstrutnov.cz>
2024-10-14 13:02:26 +0200

    piskvorky

--- 3-hry/piskvorky/Program.cs
+++ 3-hry/piskvorky/Program.cs
@@ -1,42 +1,73 @@
-﻿namespace piskvorky
+﻿using System;
+using System.Collections.Generic;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        List<int> moznosti = new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0 }; 
+        int vyherce = 0; 
+
+        while (vyherce == 0 && moznosti.Contains(0)) 
         {
+            // Hráč 1
+            Console.WriteLine("Hrac 1, vyber pole (0-8): ");
+            int hrac1 = int.Parse(Console.ReadLine());
+            if (hrac1 >= 0 && hrac1 < 9 && moznosti[hrac1] == 0)
+            {
+                moznosti[hrac1] = 1;
+            }
 
-            Console.WriteLine("herni pole:");
-            Console.WriteLine("-------------");
-            Console.WriteLine("|-1-|-2-|-3-|");
-            Console.WriteLine("|-4-|-5-|-6-|");
-            Console.WriteLine("|-7-|-8-|-9-|");
-            Console.WriteLine("-------------");
+            
+            vyherce = KontrolaVitezstvi(moznosti, 1);
+            if (vyherce != 0) break;
 
-            Console.WriteLine("hrac 1 vyber pole: ");
-            string hrac1 = Console.ReadLine();
+            // Hráč 2
+            Console.WriteLine("Hrac 2, vyber pole (0-8): ");
+            int hrac2 = int.Parse(Console.ReadLine());
+            if (hrac2 >= 0 && hrac2 < 9 && moznosti[hrac2] == 0)
+            {
+                moznosti[hrac2] = 2; 
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            
+            vyherce = KontrolaVitezstvi(moznosti, 2);
         }
+
+        
+        if (vyherce == 1)
+            Console.WriteLine("Hrac 1 vyhral!");
+        else if (vyherce == 2)
+            Console.WriteLine("Hrac 2 vyhral!");
+        else
+            Console.WriteLine("Hra skončila remízou.");
+    }
+
+    static int KontrolaVitezstvi(List<int> moznosti, int hrac)
+    {
+        
+        int[,] vyhry = {
+            {0, 1, 2}, 
+            {3, 4, 5}, 
+            {6, 7, 8}, 
+            {0, 3, 6}, 
+            {1, 4, 7}, 
+            {2, 5, 8}, 
+            {0, 4, 8}, 
+            {2, 4, 6}  
+        };
+
+        
+        for (int i = 0; i < vyhry.GetLength(0); i++)
+        {
+            if (moznosti[vyhry[i, 0]] == hrac &&
+                moznosti[vyhry[i, 1]] == hrac &&
+                moznosti[vyhry[i, 2]] == hrac)
+            {
+                return hrac;
+            }
+        }
+
+        return 0;
     }
 }
