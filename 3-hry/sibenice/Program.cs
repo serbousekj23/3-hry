4ef40cf7cfdd4455aa897b1d0978e94ac5250cb7
Honza Serbousek <serbousekj23@spstrutnov.cz>
2024-10-14 13:15:51 +0200

    sibenice zaklad

--- 3-hry/sibenice/Program.cs
+++ 3-hry/sibenice/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Sibenice! Hádej slovo: ");
+            string slova = File.ReadAllText(@"slova.txt");
+            Console.WriteLine(slova);
+        
+        
         }
     }
 }
