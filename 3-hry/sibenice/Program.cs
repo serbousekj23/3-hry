7c0f78d7b2e201717ef23bbeabef4285f9c1c3e4
Honza Serbousek <serbousekj23@spstrutnov.cz>
2024-10-18 09:35:15 +0200

    sibenice skoro

--- 3-hry/sibenice/Program.cs
+++ 3-hry/sibenice/Program.cs
@@ -5,10 +5,67 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Sibenice! Hádej slovo: ");
-            string slova = File.ReadAllText(@"slova.txt");
+            string[] slova = File.ReadAllLines(@"slova.txt");
             Random random = new Random();
+
+            int index = random.Next(slova.Length);
+            string hernislovo = slova[index];
+            List<char> pismenaslova = new List<char>(hernislovo.ToCharArray());
+            string carky = new string('_', hernislovo.Length);
+            Console.WriteLine(carky);
             
-        
+            
+            while (carky.Contains('_'))
+            {
+                Console.Write("zadej písmeno: ");
+                string input = Console.ReadLine();
+                
+                if (input.Length == 1 && char.TryParse(input, out char napsanepismeno))
+                {
+                    if (pismenaslova.Contains(napsanepismeno))
+                    {
+                        for (int i = 0; i < pismenaslova.Count; i++)
+                        {
+                            if (pismenaslova[i] == napsanepismeno)
+                            {
+                                carky[i] = napsanepismeno;
+                            }
+                        }
+                        Console.WriteLine("sigma" + new string(carky.ToArray()));
+                    }
+                    else
+                    {
+                        Console.WriteLine("spatne");
+                }    }
+                
+                else
+                {
+                    Console.WriteLine("zadej pouze 1 pismeno");
+                }
+
+
+                
+            }  
+            
+                
+            
+            
+            
+
+            
+            
+
+            
+
+            
+
+            
+            
+            
+            
+            
+            
+
         }
     }
 }
